a5048ab3c890048055d5abafbf18eb611383dca9
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-07-10 13:03:21 -0400

    feat: Add react application config.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
+
+/**
+ * Renders the application.
+ *
+ * @return {JSX.Element}
+ */
+export function App () {
+    return (
+        <div>asdf</div>
+    );
+}
---
+++ webpack.common.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: path.resolve(__dirname, "src", "index.js"),
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html"),
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "[name].[contenthash].bundle.js",
+        clean: true,
+        publicPath: "auto",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: path.resolve(__dirname, "src"),
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.ttf$/i,
+                type: "asset/resource",
+                dependency: {not: ["url"]},
+            },
+        ],
+    },
+    resolve: {},
+    optimization: {
+        moduleIds: "deterministic",
+        runtimeChunk: "single",
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "vendors",
+                    chunks: "all",
+                },
+            },
+        },
+    },
+};
